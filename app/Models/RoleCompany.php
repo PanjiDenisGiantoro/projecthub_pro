de181137dabe34849baf598a9fe029871061caf8
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2026-07-08 16:40:46 +0700

    role per company

---
+++ app/Models/RoleCompany.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Permission\Models\Role;
+
+class RoleCompany extends Model
+{
+    protected $fillable = ['role_id', 'company_id'];
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
